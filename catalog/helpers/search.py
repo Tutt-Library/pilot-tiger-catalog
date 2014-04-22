9e6859477338bcef36a2db3e305fa0d04c73c488
Jeremy Nelson <jermnelson.gmail.com>
2014-04-22 17:29:03 -0600

    added search helper and mongo_db module update

---
+++ catalog/helpers/search.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        search
+# Purpose:     Provides whoosh search for TIGER Catalog
+#
+# Author:
+#
+# Created:     2014/04/21
+# Copyright:   (c) Jeremy Nelson, Colorado College 2014
+# Licence:     GPLv2
+#-------------------------------------------------------------------------------
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
